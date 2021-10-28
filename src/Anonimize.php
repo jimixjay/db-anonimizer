d8fe0497ae810c2ff4a3569726b3b5ac9015c0a2
Jaime Martínez <jaime.martinez@tecalis.com>
2021-10-28 14:49:52 +0200

    first commit

---
+++ src/Anonimize.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Jimixjay\Anonimizer;
+
+use Exception;
+use Illuminate\Console\Command;
+use Throwable;
+
+class Anonimize extends Command
+{
+    protected $signature = "dbanonimizer:anonimize";
+    protected $description = "Anonimize DB";
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        try {
+            $path = getcwd() . DIRECTORY_SEPARATOR . 'db-anonimizer.json';
+            $configJson = file_get_contents($path);
+
+            var_dump($configJson);
+
+        } catch (Throwable $e) {
+
+        }
+    }
+
+}
